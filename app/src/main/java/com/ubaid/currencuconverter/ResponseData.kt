66815ece666074d766a20680045b65c0a19ae1e7
UbaidNiazi <53142405+Ubaid92@users.noreply.github.com>
2024-01-24 18:49:51 +0500

    some fixes

--- app/src/main/java/com/ubaid/currencuconverter/ResponseData.kt
+++ app/src/main/java/com/ubaid/currencuconverter/ResponseData.kt
@@ -2,7 +2,7 @@ import com.google.gson.annotations.SerializedName
 
 data class ResponseData(
     val date: String,
-    @SerializedName("usd", alternate = ["pkr", "gbp", "inr"])
+    @SerializedName("usd", alternate = ["pkr", "gbp", "inr","sar","omr","aed","eur"])
     val conversionResult: String
 
 )
